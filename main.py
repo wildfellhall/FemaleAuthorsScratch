d44d81cc919896ddac52c35e83d04b75cffadfc1
Wildfell Hall <136401567+wildfellhall@users.noreply.github.com>
2026-08-30 23:29:02 -0500

    Initialize main.py with dependencies and comments
    Added import and installation cell for dependencies.

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 """
 Not completed yet, but the script should be pretty similar code-wise to my Poetry50M repository. 
+As always, a line of hashtags denotes the presence of a separate cell. 
 """
+# Import/installation cell: 
+!pip install -q transformers datasets tokenizers accelerate torch
+
+import os
+import torch
+from datasets import load_dataset
+from tokenizers import ByteLevelBPETokenizer
+from transformers import PreTrainedTokenizerFast, GPT2Config, GPT2LMHeadModel, DataCollatorForLanguageModeling, Trainer, TrainingArguments
